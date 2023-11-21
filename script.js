824581e932c91d432e853d1163664986f2f9a317
jackievas <145609624+jackievas@users.noreply.github.com>
2023-11-21 13:21:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Schedule Report</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <div id="scheduleReport"></div>
+// Load employee data from JSON file
+fetch('employee.json')
+  .then(response => response.json())
+  .then(employeeData => {
+    // Load schedule data from XML file
+    fetch('schedule.xml')
+      .then(response => response.text())
+      .then(scheduleData => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(scheduleData, "application/xml");
 
-  <script src="script.js"></script>
-</body>
-</html>
+        const shifts = xmlDoc.querySelectorAll("shift");
+        const scheduleReportDiv = document.getElementById("scheduleReport");
+
+        shifts.forEach((shift) => {
+          const employeeId = shift.querySelector("employeeId").textContent;
+          const day = shift.querySelector("day").textContent;
+          const startTime = shift.querySelector("startTime").textContent;
+          const endTime = shift.querySelector("endTime").textContent;
+
+          const employee = employeeData.employees.find((emp) => emp.id.toString() === employeeId);
+
+          if (employee) {
+            const scheduleInfo = `
+              <p>${employee.name} (${employee.position}, ${employee.department})</p>
+              <p>Day: ${day}</p>
+              <p>Shift: ${startTime} - ${endTime}</p>
+              <hr>
+            `;
+            scheduleReportDiv.innerHTML += scheduleInfo;
+          }
+        });
+      })
+      .catch(error => console.error('Error loading schedule data:', error));
+  })
+  .catch(error => console.error('Error loading employee data:', error));
+
