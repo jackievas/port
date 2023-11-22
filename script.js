299e3621dcf7c8371a5c2980851873f059b8caa3
jackievas <145609624+jackievas@users.noreply.github.com>
2023-11-22 16:55:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 // Create cells for all days of the week
                 daysOfWeek.forEach(() => {
-                  scheduleRow.insertCell();
+                  const cell = scheduleRow.insertCell();
+                  cell.classList.add('scheduleCell'); // Add class to all cells
                 });
               }
 
